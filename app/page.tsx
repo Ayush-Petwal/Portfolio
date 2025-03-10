2bef5eb45e1f4f5f0953f3912ce2686b45146129
Ayush-Petwal <ayushpetwal003@gmail.com>
2025-03-11 02:14:12 +0530

    Projects and Skills section done

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,29 @@
+"use client";
+
+import { navItems } from "@/data";
+
 import Hero from "@/components/Hero";
+import Grid from "@/components/Grid";
+import Footer from "@/components/Footer";
+import Approach from "@/components/Approach";
+import Skills from "@/components/Skills";
+import RecentProjects from "@/components/RecentProjects";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
 
-export default function Home() {
+const Home = () => {
   return (
-      <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden m-auto sm:px-10 px-5">
-        
-        <div className="max-w-7xl w-full ">
-
-          <Hero />
-
-        </div>
-
-      </main>
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
+        <Hero />
+        <Grid />
+        <Skills/>
+        <RecentProjects />
+        <Approach />
+        <Footer />
+      </div>
+    </main>
   );
-}
+};
+
+export default Home;
