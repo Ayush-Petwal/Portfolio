52cc41e5d06f3866a4f22d8083af6c259e65658c
Ayush-Petwal <ayushpetwal003@gmail.com>
2025-03-11 19:06:55 +0530

    fix some deployment bug

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -23,7 +23,7 @@ const Footer = () => {
           />
         </a>
       </div>
-      <div className="flex mt-16 md:flex-row flex-col justify-between items-center mb-24">
+      <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
         <p className="md:text-base text-sm md:font-normal font-light">
           Copyright © 2025 Ayush Petwal
         </p>
