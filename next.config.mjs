aa29046810082e21c960b7e06de2abc76238c75f
Ayush-Petwal <ayushpetwal003@gmail.com>
2025-03-11 19:20:08 +0530

    fix errors

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,15 @@ const nextConfig = {
   },
   sentry: {
     ignoreErrors: true,
-  }
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      console.log("Running on the server");
+    } else {
+      console.log("Running on the client");
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
